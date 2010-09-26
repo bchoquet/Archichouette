6b1bab7202b2043928e652eb199418c5e1aa5013
Benjamin Choquet <bchoquet@gmail.com>
2010-09-26 16:49:30 +0200

    deplacement de la config siteaccess dans settings

---
+++ settings/siteaccess/fre/design.ini.append.php
@@ -0,0 +1,10 @@
+<?php /* #?ini charset="utf-8"?
+
+[StylesheetSettings]
+SiteCSS=var/storage/packages/eZ-systems/ezflow_design/files/default/file/site-colors.css
+ClassesCSS=var/storage/packages/eZ-systems/ezflow_design/files/default/file/classes-colors.css
+CSSFileList[]
+
+[JavaScriptSettings]
+JavaScriptList[]=insertmedia.js
+*/ ?>
