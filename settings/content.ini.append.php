7c6c3e443e139c9516f6f68ad21128adfacb6f3c
Benjamin Choquet <bchoquet@gmail.com>
2010-11-01 15:05:52 +0100

    Ajout du style vert dans l'OE et les styles

---
+++ settings/content.ini.append.php
@@ -0,0 +1,8 @@
+<?php /* #?ini charset="utf-8"?
+[literal]
+AvailableClasses[]=vert
+
+[paragraph]
+AvailableClasses[]=vert
+
+?>
---
+++ settings/siteaccess/admin_archi/design.ini.append.php
@@ -0,0 +1,5 @@
+<?php /* #?ini charset="utf-8"?
+
+[StylesheetSettings]
+EditorCSSFileList[]=oestyles.css
+*/ ?>
