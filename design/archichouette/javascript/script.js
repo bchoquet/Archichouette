3196d749719e4cf6868651d881072e0fdc6b13e4
Benjamin Choquet <bchoquet@gmail.com>
2010-11-06 18:57:35 +0100

    Pas de slideshow si moins de 2 visuels

--- design/archichouette/javascript/script.js
+++ design/archichouette/javascript/script.js
@@ -3,10 +3,15 @@
  */
 jQuery(function(){
 	//slideshow avec jQuery Cycle
-	jQuery(".slideshow").append('<a href="#" class="prev-arrow"></a><a href="#" class="next-arrow"></a>');
-	jQuery(".slideshow-contents").cycle({
-							height: "400px",
-							next: ".slideshow .next-arrow",
-							prev: ".slideshow .prev-arrow"
-						});
+	jQuery(".slideshow").each(function(){
+		var $this = $(this);
+		if($this.find(".slideshow-contents").children().length < 2) return;
+		$this.append('<a href="#" class="prev-arrow"></a><a href="#" class="next-arrow"></a>');
+		$this.find(".slideshow-contents").cycle({
+								height: "400px",
+								next: ".slideshow .next-arrow",
+								prev: ".slideshow .prev-arrow"
+							});
+	});
+	
 });
