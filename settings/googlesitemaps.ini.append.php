0ee8916839153d02fdd3e4d0697bb6ba3c540f09
Benjamin Choquet <bchoquet@gmail.com>
2010-11-20 19:10:28 +0100

    config du sitemap

---
+++ settings/googlesitemaps.ini.append.php
@@ -0,0 +1,11 @@
+<?php /*
+[Classes]
+ClassFilterType=include
+ClassFilterArray[]
+ClassFilterArray[]=frontpage
+ClassFilterArray[]=jeu
+ClassFilterArray[]=event
+ClassFilterArray[]=event_calendar
+ClassFilterArray[]=categorie
+
+*/ ?>
