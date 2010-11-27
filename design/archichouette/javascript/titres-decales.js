7192d0340d0fa311b6581beb11eea88d3137c5f2
unknown <pedro@.(none)>
2010-11-27 15:35:38 +0100

    ajout d'un timeout sur le decalage des titres pour gerer le temps de chargement de la typo

--- design/archichouette/javascript/titres-decales.js
+++ design/archichouette/javascript/titres-decales.js
@@ -1,13 +1,17 @@
 (function($, undefined){
 		$.fn.archiTitre = function(){
+		
 		this.each(function(){
 			var $this = $(this);
-			$this.css({
-				width: $this.width(),
-				height: Math.round($this.height()/3*2)
-			});
-			$this.addClass("archiTitre");
-			$this.contents().wrapAll('<span class="decalage"></span>');
+			setTimeout(function(){
+				
+				$this.css({
+					width: $this.width(),
+					height: Math.round($this.height()/3*2)
+				});
+				$this.addClass("archiTitre");
+				$this.contents().wrapAll('<span class="decalage"></span>');
+			}, 100);
 		});
 	}
 })(window.jQuery);
