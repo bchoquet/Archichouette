2d4df0685a0d42477bbb022de4a621a88a6c7e38
unknown <pedro@.(none)>
2010-11-27 17:13:09 +0100

    installation et configuration de ngpush

---
+++ settings/ngpush.ini.append.php
@@ -0,0 +1,26 @@
+<?php /* #?ini charset="utf-8"?
+
+[PushNodeSettings]
+SiteURL=www.benjamin-choquet.fr/archichouette/index.php?/fre
+ConnectURL=push.netgen.hr
+Blocks[]
+Blocks[]=facebook
+ActiveBlocks[]
+ActiveBlocks[]=facebook
+
+[facebook]
+Id=173626272667499
+attrId_name[]
+attrId_name[jeu]=title
+attrId_name[event]=title
+attrId_description[]
+attrId_description[jeu]=intro
+attrId_description[event]=text
+attrId_message[]
+attrId_message[jeu]=body
+attrId_message[event]=text
+attrId_picture[]
+attrId_picture[jeu]=visuels
+attrId_picture[event]=visuels
+
+*/ ?>
